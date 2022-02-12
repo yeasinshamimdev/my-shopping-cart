924d0557f48840f8c952801488d9bac1c9fe41b3
Yeasin <yeasinshamim7@gmail.com>
2022-02-13 00:57:20 +0600

    case total added

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-document.getElementById('case-plus').addEventListener('click', function(){
+function updateCaseNumber(isIncise){
     const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
+    if(isIncise){
+        caseInput.value = parseInt(caseNumber) + 1;
+    }
+    else if(caseNumber > 0){
+        caseInput.value = parseInt(caseNumber) - 1;
+    }
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseInput.value * 59;
+}
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateCaseNumber(true);
+});
+document.getElementById('case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
 })
