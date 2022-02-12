d84549ce5e10e9057e8f4f5f0beb74743dc7dbbb
Yeasin <yeasinshamim7@gmail.com>
2022-02-13 02:52:07 +0600

    CURD project done

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
-function updateCaseNumber(isIncise){
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncise){
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = productInput.value;
     if(isIncise){
-        caseInput.value = parseInt(caseNumber) + 1;
+        productInput.value = parseInt(productNumber) + 1;
     }
-    else if(caseNumber > 0){
-        caseInput.value = parseInt(caseNumber) - 1;
+    else if(productNumber > 0){
+        productInput.value = parseInt(productNumber) - 1;
     }
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseInput.value * 59;
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productInput.value * price;
+    calculateTotal()
 }
+function inputValue(product){
+    const  productInput = document.getElementById(product + '-number');
+    const parseProduct = parseInt(productInput.value);
+    return parseProduct;
+}
+function calculateTotal(){
+    const caseTotal = inputValue('case') * 59;
+    const phoneTotal = inputValue('phone') * 1219;
+
+    const subTotal = caseTotal + phoneTotal;
+    const tax = subTotal / 10;
+    const total = subTotal + tax;
+
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = total;
+}
+
+// phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click',function(){
+    updateProductNumber('phone',1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updateProductNumber('phone', 1219, false);
+})
+
+// case increase decrease events
 document.getElementById('case-plus').addEventListener('click', function(){
-    updateCaseNumber(true);
+    updateProductNumber('case', 59, true);
 });
 document.getElementById('case-minus').addEventListener('click', function(){
-    updateCaseNumber(false);
-})
+    updateProductNumber('case', 59, false);
+});
